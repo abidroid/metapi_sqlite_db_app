824660d5d5ad55acc22a99e21f1013ed51418508
Abid <abidroid1@gmail.com>
2026-04-14 18:06:42 +0500

    listview builder for students

--- lib/pages/student_list_page.dart
+++ lib/pages/student_list_page.dart
@@ -20,8 +20,71 @@ class _StudentListPageState extends State<StudentListPage> {
         (future: DatabaseHelper.instance.getAllStudents(),
           builder: (context, snapshot){
         if( snapshot.hasData){
+         // return Center(child: Text(snapshot.data!.length.toString()));
 
-          return Center(child: Text(snapshot.data!.length.toString()));
+
+          List<Map<String, dynamic>> students = snapshot.data!;
+
+
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView.builder(
+                itemCount: students.length,
+                itemBuilder: (context, index){
+
+                  Map<String, dynamic> student = students[index];
+
+                  return Card(
+                    color: Colors.amber[100],
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        spacing: 16,
+                        children: [
+                          Text('CNIC: ${student['cnic']}'),
+                          Text('Name: ${student['name']}'),
+                          Text('Course: ${student['course']}'),
+                          Text('Qual: ${student['lastQualification']}'),
+
+                          Row(
+                            spacing: 16,
+                            children: [
+                              Expanded(
+                                child: ElevatedButton.icon(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.red,
+                                    foregroundColor: Colors.white
+                                  ),
+
+                                  onPressed: (){},
+                                    label: Text('Delete'),
+                                    icon: Icon(Icons.delete,),
+                                ),
+                              ),
+
+                              Expanded(
+                                child: ElevatedButton.icon(
+                                  style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.green,
+                                      foregroundColor: Colors.white
+                                  ),
+
+                                  onPressed: (){},
+                                  label: Text('Update'),
+                                  icon: Icon(Icons.edit),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+
+            }),
+          );
+
         }else{
           return Center(child: CircularProgressIndicator());
         }
